3bb4df228d9c89f8c42571f20ea48bcd1f392c32
ZetaSploit <lazulilapis852@gmail.com>
2026-08-24 14:04:49 +0530

    Resolving bug currently...

--- src/bitjecta.cc
+++ src/bitjecta.cc
@@ -1,10 +1,16 @@
+#include <cstddef>
 #include <ios>
 #include <iostream>
 #include <fstream>
+#include <iterator>
 #include <vector>
 #include <string>
 #include <iomanip>
 #include <filesystem>
+#include <byteswap.h>
+#include <bit>
+//#include <cstdint>
+
 
 
 void fparse(const std::string& file_name) {
@@ -21,28 +27,38 @@ void fparse(const std::string& file_name) {
     std::streamsize total_size = file.tellg();
 
     file.seekg(0, std::ios::beg);
-    std::vector<char> buffer(total_size);
-
-    
+    std::vector<char> buffer(total_size); 
     file.read(buffer.data(), total_size);
 
     std::streamsize bytes_read = file.gcount();
 
-    for (std::streamsize i = 0; i < bytes_read; ++i) { 
-
+    for (std::streamsize i = 0; i < bytes_read; ++i) {
         std::cout << std::hex << std::setw(2) << std::setfill('0') << (static_cast<unsigned int>(static_cast<unsigned char>(buffer[i]))) << " ";
+
         if ((i + 1) % 16 == 0)
             std::cout << '\n';
     }
 
     std::cout << '\n';
 }
+
+std::vector<std::uint32_t> convert_and_swap(const std::vector<char>& buffer) {
+    std::size_t num_elements = buffer.size() / 4;
+    std::vector<std::uint32_t> result(num_elements);
+
+    std::memcpy(result.data(), buffer.data(), num_elements * 4);
+
+    for (std::size_t i = 0; i < result.size(); i++) {
+        result[i] = std::byteswap(result[i]);
+    }
+
+    return result;
+}
+
+
 int main(int argc, char *argv[]) {
 
-    char user_def[100];
-
-    //std::cout << "Enter the file to be parsed: ";
-    //std::cin >> user_def;
+    char user_def[100]; 
 
     fparse(argv[1]);
 
